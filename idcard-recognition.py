b23ce651cfc056b7b6c94ae6ef8888a978a6cd61
FlowerF <1647231392@qq.com>
2021-11-22 09:24:41 +0800

    fix some bugs

--- idcard-recognition.py
+++ idcard-recognition.py
@@ -25,27 +25,32 @@ kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
 # 灰度化
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 # 二值化
 _, binary_img = cv2.threshold(gray_img, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 # binary_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 155, 1)
 
-# 腐蚀
+# 腐蚀 待调参
 erode_img = cv2.erode(binary_img, kernel, iterations=6)
 
-# 闭操作
+# 闭操作 待调参
 close_img = cv2.morphologyEx(erode_img, cv2.MORPH_CLOSE, kernel,iterations=2)
 
 # 轮廓查找
 contours, _ = cv2.findContours(close_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# 遍历轮廓
+# 记录所有符合条件的矩形
 rects = []
+
+# 遍历轮廓
 for (_, contour) in enumerate(contours):
     # 求出宽高
     x, y, w, h = cv2.boundingRect(contour)
     # 在符合条件的基础上 进行矩形框绘制
     if (w > h * 6) and (w < h * 18) and (w * h >= 3000):
+        # 绘制红框
         rect = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # 把矩形添加到数组中
         rects.append({'x': x, 'y': y, 'w': w, 'h': h})
 
 if rects.__len__() < 1:
@@ -62,14 +67,16 @@ for (_, rect) in enumerate(rects):
 # dilate_img = cv2.dilate(binary_img, kernel, iterations=1)
 
 # 截取区域
-dst = binary_img[final_rect['y']:(final_rect['y']+final_rect['h']), final_rect['x']:(final_rect['x']+final_rect['w'])]
+dst = img[final_rect['y']:(final_rect['y']+final_rect['h']), final_rect['x']:(final_rect['x']+final_rect['w'])]
 
 # 获取文本
+# 待调参
 # text = pytesseract.image_to_string(dst, lang="chi_sim+eng", config='--psm 7 --oem 3')
 # text = pytesseract.image_to_string(dst, lang="eng", config='--psm 8')
 text = pytesseract.image_to_string(dst, lang="eng", config='--psm 7 --oem 3')
 
 print(text.replace(' ', ''))
+print('识别结果未必精确，请再仔细确认号码是否无误')
 cv2.imshow('img', img)
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
